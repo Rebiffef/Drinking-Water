9da9a457b5185f6573054bc1712d2582430b7fde
Rebiffef <fanny.rebiffe@gmail.com>
2021-01-05 17:12:32 +0000

    data initiale contenant des confusions entre observations et variables modifications opérées : pivot transformation

---
+++ StabilitePolitique_data_cleaning.R
@@ -0,0 +1,8 @@
+StabPol <- as.data.frame(read.csv("StabilitePolitique.csv",dec=","))
+
+StabPol<-StabPol[,!(colnames(StabPol) %in% c("Element.Code","Element","Note","Flag","Flag.Description","Item.Code","Unit"))]
+
+StabPol<-StabPol %>% pivot_wider(names_from = "Item",values_from="Value")
+
+write.csv(EauPotable,"StabilitePolitique.csv")
+
