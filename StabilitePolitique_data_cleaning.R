4c835db5e428d4efa419e70c68524353cce1a7cf
Rebiffef <75374648+Rebiffef@users.noreply.github.com>
2021-01-28 11:13:59 +0100

    Delete StabilitePolitique_data_cleaning.R

--- StabilitePolitique_data_cleaning.R
+++
@@ -1,8 +0,0 @@
-StabPol <- as.data.frame(read.csv("StabilitePolitique.csv",dec=","))
-
-StabPol<-StabPol[,!(colnames(StabPol) %in% c("Element.Code","Element","Note","Flag","Flag.Description","Item.Code","Unit"))]
-
-StabPol<-StabPol %>% pivot_wider(names_from = "Item",values_from="Value")
-
-write.csv(StabPol,"StabilitePolitique.csv")
-
